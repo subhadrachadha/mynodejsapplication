adf18c063ded5ee3f2bb659f55e4653aeb2b99cf
subhadrachadha <subhadrachadha88@gmail.com>
2019-10-09 16:59:29 +0530

    Update startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.cohobyukovto.ap-south-1.rds.amazonaws.com
+export DB_PRD_HOST=dellstore2.cmwhiveil5ia.us-east-2.rds.amazonaws.com
 export DB_PRD_USER=dellstoreaws
-export DB_PRD_PASS=dellstoreaws
+export DB_PRD_PASS=manu1234
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
